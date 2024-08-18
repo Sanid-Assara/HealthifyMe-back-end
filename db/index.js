7af696c7a02c005a2f103b8dd59e667faf1a56fe
Sanid-Assara <sanid.assara@gmail.com>
2024-08-18 21:27:47 +0200

    Add collections test for atlas db

--- db/index.js
+++ db/index.js
@@ -1,9 +1,16 @@
 import mongoose from "mongoose";
+import User from "../models/userModel.js";
+import Food from "../models/foodModel.js";
+import Recipe from "../models/recipeModel.js";
 
 const connectToDatabase = async () => {
   try {
     await mongoose.connect(process.env.MONGO_URI);
     console.log(" Connected to MongoDB!");
+    await User.ensureIndexes();
+    await Food.ensureIndexes();
+    await Recipe.ensureIndexes();
+    console.log(" collections should exist now on Atlas.");
   } catch (error) {
     console.error(" Failed to connect to MongoDB", error);
     throw error;
