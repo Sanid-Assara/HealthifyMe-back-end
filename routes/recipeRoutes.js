d0ad0a8d3468dd27f2895c5a4ca4d699ff08f684
Sanid-Assara <sanid.assara@gmail.com>
2024-08-20 14:48:00 +0200

    Create the recipeController
    Update the recipeRoutes and connect it to the controller

--- routes/recipeRoutes.js
+++ routes/recipeRoutes.js
@@ -1,44 +1,11 @@
 import express from "express";
-
-const getRecipes = async (req, res) => {
-  try {
-    res.send("get request for recipes");
-  } catch (error) {
-    res.status(500).json({ error: error.message });
-  }
-};
-
-const createRecipe = async (req, res) => {
-  try {
-    res.send("post request for a recipe");
-  } catch (error) {
-    res.status(500).json({ error: error.message });
-  }
-};
-
-const getRecipe = async (req, res) => {
-  try {
-    res.send("get request for a single recipe");
-  } catch (error) {
-    res.status(500).json({ error: error.message });
-  }
-};
-
-const updateRecipe = async (req, res) => {
-  try {
-    res.send("put request for a recipe");
-  } catch (error) {
-    res.status(500).json({ error: error.message });
-  }
-};
-
-const deleteRecipe = async (req, res) => {
-  try {
-    res.send("delete request for a recipe");
-  } catch (error) {
-    res.status(500).json({ error: error.message });
-  }
-};
+import {
+  getRecipes,
+  createRecipe,
+  getRecipe,
+  updateRecipe,
+  deleteRecipe,
+} from "../controllers/recipeController.js";
 
 const recipeRoutes = express.Router();
 
